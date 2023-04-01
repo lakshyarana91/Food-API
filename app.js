ecdb1d7e89a418bcde854f031b315f5395b1373a
Vivek Singh <viveksingh27795@gmail.com>
2023-04-01 17:36:18 +0530

    fix

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const morgan = require('morgan');
 const dishRouter = require('./router/dishRouter');
 
 const app = express();
+const  cors = require('cors');
+
 
 //MIDDLEWARES
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+app.use(cors());
+app.options('*', cors());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-
 const DB = process.env.DATABASE
 
 mongoose.connect(DB, {
